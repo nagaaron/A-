71c64192e71ef6cf40af7fe5ff610f0f7de30eb6
Aaron Nagel <49287509+nagaaron@users.noreply.github.com>
2020-04-16 21:40:03 +0200

    Add files via upload

---
+++ A_starr.py
@@ -0,0 +1,67 @@
+# implementing A star algorithm
+import random
+import pygame
+
+screen_width = 500
+field_size = 10
+fields = screen_width/field_size
+
+class Spot:
+    def __init__(self,i,j):
+        self.x = i
+        self.y = j
+        self.g = ((self.x-0)*2+(self.y-0)**2)**0.5
+        self.h = ((self.x-10)*2+(self.y-10)**2)**0.5
+        self.f = self.g + self.h
+    
+    def draw(self,window):
+        pygame.draw.rect(window,(255,255,255),(self.x,self.y,fields,fields))
+
+def distance_between_points(point_a,point_b):
+    return ((point_a[1]-point_b[1])**2+(point_a[0]+point_b[0])**2)**0.5
+
+def draw_points(liste,window):
+    window.fill((0,0,0))
+    for i in range(0,500,50):
+        pygame.draw.line(window,(0,255,0),(0,i),(500,i))
+        pygame.draw.line(window,(0,255,0),(i,0),(i,500))
+    for i in liste:
+        i.draw(window)                        
+
+def main_loop():
+    start_point = Spot(0,0)
+    end_point = Spot(fields-1,fields-1)
+    
+    open_list = []
+    closed_list = []
+    
+    open_list.append(start_point)
+        
+    clock = pygame.time.Clock()
+    window = pygame.display.set_mode((screen_width,screen_width))
+    pygame.display.set_caption("A* Pathfinder")
+    
+    while len(open_list)>0:
+        clock.tick(10)
+        
+        draw_points(open_list,window)
+        closed_list.append(open_list.pop())
+        pygame.display.update()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                
+main_loop()      
+
+
+
+
+
+
+
+
+
+    
+    
+
